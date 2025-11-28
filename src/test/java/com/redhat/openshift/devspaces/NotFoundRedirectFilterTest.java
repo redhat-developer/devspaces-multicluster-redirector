304e5ef24e854ffdcb8bd7057803a7e8d359d6ff
Ilya Buziuk <ibuziuk@redhat.com>
2025-11-28 11:22:26 +0000

    Implement 404 redirect filter
    Assisted-by: Roo Code
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

---
+++ src/test/java/com/redhat/openshift/devspaces/NotFoundRedirectFilterTest.java
@@ -0,0 +1,21 @@
+package com.redhat.openshift.devspaces;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class NotFoundRedirectFilterTest {
+
+    @Test
+    public void testNotFoundRedirect() {
+        given()
+          .redirects().follow(false) // Disable automatic redirect following to check the 302 response
+          .when().get("/non-existent-path")
+          .then()
+             .statusCode(302)
+             .header("Location", is("/"));
+    }
+}
